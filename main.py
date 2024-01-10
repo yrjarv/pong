50b26e45198420fa0fc65fc9a0af4b0b15307665
Yrjar V <yrjarvederhus@gmail.com>
2024-01-10 08:40:55 +0100

    Upload all files

---
+++ main.py
@@ -0,0 +1,115 @@
+"""
+Main program for the game Pong
+"""
+# pylint: disable=E1101
+import random
+import pygame
+import shapes
+
+# Constants
+PADDLE_SPEED = 0.75
+WINDOW = (800, 600)
+TPS = 200
+BACKGROUNDCOLOR = (0, 0, 0)
+GAMECOLOR = (255, 255, 255)
+
+# Starting values
+player_scores = [0, 0]
+
+# Functions to randomize ball behaviour
+def random_start_ball_speed() -> tuple[float, float]:
+    """
+    Generates a tuple with two random numbers, representing the vector of\
+    the ball's speed.
+    :returns tuple[float, float]: The vector of the ball's speed
+    """
+    return (
+        random.choice([-1, 1]) * random.uniform(0.3, 0.75) * 0.5,
+        random.choice([-1, 1]) * random.uniform(0.1, 0.3)
+    )
+
+def random_paddle_collision_ball_speed(previous_ball_speed: tuple[float, float]
+                                       ) -> tuple[float, float]:
+    """
+    Generates a tuple with two numbers that are the result of random\
+    multiplication of the previous ball speed.
+    :param tuple[float, float] previous_ball_speed: The previous ball speed
+    :returns tuple[float, float]: The new ball speed
+    """
+    x_multiplier = random.uniform(0.95, 1.15)
+    return (
+        previous_ball_speed[0] * -1 * x_multiplier,
+        previous_ball_speed[1] * random.choice((-1, 1)) * x_multiplier * 0.8
+    )
+
+# Initialize pygame, set up window and tick speed
+pygame.init()
+pygame.time.Clock().tick(TPS)
+window = pygame.display.set_mode(WINDOW)
+window.fill(BACKGROUNDCOLOR)
+font = pygame.font.SysFont('Arial', 48)
+
+# Initialise all elements
+paddles = [
+    shapes.Paddle([20, 250], window, color=GAMECOLOR),
+    shapes.Paddle([760, 250], window, color=GAMECOLOR)
+]
+ball = shapes.Ball([400, 300], window)
+ball_speed = random_start_ball_speed()
+ball.position = [window.get_width()/2, window.get_height()/2]
+
+# Game loop
+while True:
+    # Break loop if user closes pygame window
+    if pygame.event.get(pygame.QUIT):
+        break
+
+    # Get key input
+    keys = pygame.key.get_pressed()
+    # Moving according to input
+    if keys[pygame.K_w]:
+        paddles[0].move(-PADDLE_SPEED)
+    if keys[pygame.K_s]:
+        paddles[0].move(PADDLE_SPEED)
+    if keys[pygame.K_UP]:
+        paddles[1].move(-PADDLE_SPEED)
+    if keys[pygame.K_DOWN]:
+        paddles[1].move(PADDLE_SPEED)
+
+    # Movement and multiplication/randomisation of speed
+    if ball.collision_wall():
+        ball_speed = (ball_speed[0], ball_speed[1]*random.uniform(-1.2, -0.95))
+    if ball.collision_paddle(paddles):
+        ball_speed = random_paddle_collision_ball_speed(ball_speed)
+    ball.move(ball_speed)
+
+    # Checking winners, changing scores and resetting the ball
+    if not (0 < ball.position[0] < window.get_width()
+            and -10 < ball.position[1] < window.get_height()+10):
+        if ball.position[0] <= 0:
+            player_scores[1] += 1
+        elif ball.position[0] >= window.get_width():
+            player_scores[0] += 1
+        ball.position = [window.get_width()/2, window.get_height()/2]
+        ball_speed = random_start_ball_speed()
+
+    # Draw all objects and update score text
+    for paddle in paddles:
+        paddle.draw()
+    ball.draw()
+    text = font.render(f'{player_scores[0]} : {player_scores[1]}', True,
+                       GAMECOLOR)
+    text_rect = text.get_rect(center=(window.get_width()/2, 50))
+    window.blit(text, text_rect)
+    pygame.display.update()
+    window.fill(BACKGROUNDCOLOR)
+
+    # Quit if esc is pressed, done after the rendering to ensure that all
+    # objects are still drawn
+    if keys[pygame.K_ESCAPE]:
+        break
+
+# Game done
+while not pygame.event.get(pygame.QUIT):
+    continue
+pygame.quit() # Quits only when pygame window is closed
---
+++ shapes.py
@@ -0,0 +1,130 @@
+"""
+Contains the shapes used in Pong
+"""
+import pygame
+pygame.init() # pylint: disable=no-member
+
+class Paddle:
+    """
+    Represents a paddle for the player to move in the game. Ball objects\
+    collide with Paddle objects
+    """
+    def __init__(self, position: list[float], window,
+                 dimensions: tuple[float, float] = (20, 100),
+                 color: tuple = (255, 255, 255)) -> None:
+        """
+        :param list[float] position: Contains the x- and y-values for the\
+        rectangle to start at, starting at the top left corner of the window
+        :param window: A Pygame window to draw the paddles in
+        :param tuple[float] dimensions: Contains respectively the width and\
+        height of the paddle
+        :param tuple color: The RGB value of the paddle's color, default is\
+        white
+        """
+        self.position = position
+        self.window = window
+        self.dimensions = dimensions
+        self.color = color
+
+        self.draw()
+
+
+    def draw(self) -> None:
+        """
+        Draws the rectangle on self.window
+        """
+        pygame.draw.rect(self.window, self.color,
+                            (self.position[0], self.position[1],
+                             self.dimensions[0], self.dimensions[1])
+                        )
+
+    def collision_with_wall(self, added_distance: float) -> bool:
+        """
+        Checks if the paddle collides with the top or bottom of the screen
+        :return: True if the paddle collides with the top or bottom of the\
+        screen, False otherwise
+        """
+        new_y = self.position[1] + added_distance
+        return (new_y <= 0 or
+                new_y+self.dimensions[1] >= self.window.get_height())
+
+    def move(self, distance: float) -> None:
+        """
+        Moves the paddle a given amount y direction as long as that doesn't\
+        make it collide witht the walls
+        :param float distance: How much the paddle should move
+        """
+        if not self.collision_with_wall(distance):
+            self.position[1] += distance
+        self.draw()
+
+
+class Ball:
+    """
+    Represents a ball in the game. Ball objects collide with Paddle objects
+    """
+    def __init__(self, position: list[float], window, radius: int = 10,
+                 color: tuple = (255, 255, 255)) -> None:
+        """
+        :param list[float] position: Contains the x- and y-values for the\
+        rectangle to start at, starting at the top left corner of the window
+        :param window: A Pygame window to draw the ball in
+        :param int radius: The radius of the ball
+        :param tuple color: The RGB value of the ball's color, default is\
+        white
+        """
+        self.window = window
+        self.radius = radius
+        self.position = position
+        self.color = color
+
+        self.draw()
+
+    def draw(self) -> None:
+        """
+        Draws the ball on self.window
+        """
+        pygame.draw.circle(self.window, self.color, self.position, self.radius)
+
+    def collision_paddle(self, paddles: list) -> bool:
+        """
+        Checks if the ball collides with any of the paddles
+        :param list paddles: A list of Paddle objects
+        :return: True if the ball collides with any of the paddles, False\
+        otherwise
+        """
+        return (
+            (
+                self.position[0] < paddles[0].position[0]+paddles[0].dimensions[0]
+                and paddles[0].position[1] < self.position[1]
+                < paddles[0].position[1]+paddles[0].dimensions[1]
+            )
+            or
+            (
+                self.position[0] > paddles[1].position[0]
+                and paddles[1].position[1] < self.position[1]
+                < paddles[1].position[1]+paddles[1].dimensions[1]
+            )
+
+        )
+
+    def collision_wall(self) -> bool:
+        """
+        Checks if the ball collides with the top or bottom of the screen
+        :return: True if the ball collides with the top or bottom of the\
+        screen, False otherwise
+        """
+        return (
+            self.position[1] <= 0 # top
+            or self.position[1] >= self.window.get_height() # bottom
+        )
+
+    def move(self, distance: tuple[float, float]) -> None:
+        """
+        Moves the ball
+        :param tuple[float, float] distance: How much the ball should move in\
+        x and y directions
+        """
+        self.position[0] += distance[0]
+        self.position[1] += distance[1]
+        self.draw()
